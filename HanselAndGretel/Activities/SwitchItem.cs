cb53056a8425f9ad69f369453c555f6366cae1d5
sensorreinigung <jens@richter-fam.de>
2014-06-04 00:09:39 +0200

    ItemHandler

---
+++ HanselAndGretel/Activities/SwitchItem.cs
@@ -0,0 +1,110 @@
+﻿using HanselAndGretel.Data;
+using KryptonEngine.Entities;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HanselAndGretel
+{
+	class SwitchItem : ActivityState
+	{
+		#region Properties
+
+		protected const float MaxSwapDistance = 100f;
+		protected int InventoryFocusHansel;
+		protected int InventoryFocusGretel;
+
+		#endregion
+
+		#region Constructor
+
+		public SwitchItem(Hansel pHansel, Gretel pGretel)
+			: base(pHansel, pGretel)
+		{
+			Initialize();
+		}
+
+		#endregion
+
+		#region OverrideMethods
+
+		public override void Initialize()
+		{
+			InventoryFocusHansel = 1;
+			InventoryFocusGretel = 1;
+		}
+
+		public override void PrepareAction(Player pPlayer)
+		{
+			if ((rHansel.Position - rGretel.Position).Length() <= MaxSwapDistance)
+				m2ndState = true;
+			else
+				m2ndState = false;
+			if (pPlayer.GetType() == typeof(Hansel))
+			{
+				InventoryFocusHansel = 1;
+				mStateHansel = State.Running;
+			}
+			else if (pPlayer.GetType() == typeof(Gretel))
+			{
+				InventoryFocusGretel = 1;
+				mStateGretel = State.Running;
+			}
+		}
+
+		public override void StartAction(Player pPlayer)
+		{
+			//Nothing to do here!
+		}
+
+		public override void UpdateAction(Player pPlayer)
+		{
+			if (pPlayer.GetType() == typeof(Hansel))
+			{
+				//Navigate Inventory
+				if (pPlayer.Input.ItemLeftJustPressed)
+					--InventoryFocusHansel;
+				if (pPlayer.Input.ItemRightJustPressed)
+					++InventoryFocusHansel;
+				MathHelper.Clamp(InventoryFocusHansel, 0, 3);
+
+				if (pPlayer.Input.ActionJustPressed && m2ndState)
+					SwapItem(pPlayer);
+			}
+			else if (pPlayer.GetType() == typeof(Gretel))
+			{
+				//Navigate Inventory
+				if (pPlayer.Input.ItemLeftJustPressed)
+					--InventoryFocusGretel;
+				if (pPlayer.Input.ItemRightJustPressed)
+					++InventoryFocusGretel;
+				MathHelper.Clamp(InventoryFocusGretel, 0, 3);
+
+				if (pPlayer.Input.ActionJustPressed && m2ndState)
+					SwapItem(pPlayer);
+			}
+		}
+
+		protected void SwapItem(Player pPlayer)
+		{
+			if (pPlayer.GetType() == typeof(Hansel))
+			{
+				if (rHansel.Inventory.ItemSlots[InventoryFocusHansel].Item == null)
+					return;
+				if (rGretel.Inventory.TryToStore(rHansel.Inventory.ItemSlots[InventoryFocusHansel].Item))
+					rHansel.Inventory.ItemSlots[InventoryFocusHansel].Item = null;
+			}
+			else if (pPlayer.GetType() == typeof(Gretel))
+			{
+				if (rGretel.Inventory.ItemSlots[InventoryFocusGretel].Item == null)
+					return;
+				if (rHansel.Inventory.TryToStore(rGretel.Inventory.ItemSlots[InventoryFocusGretel].Item))
+					rGretel.Inventory.ItemSlots[InventoryFocusGretel].Item = null;
+			}
+		}
+
+		#endregion
+	}
+}
--- HanselAndGretel/Logic/ItemHandler.cs
+++ HanselAndGretel/Logic/ItemHandler.cs
@@ -1,4 +1,7 @@
 ﻿using HanselAndGretel.Data;
+using KryptonEngine;
+using KryptonEngine.Entities;
+using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +11,14 @@ namespace HanselAndGretel
 {
 	public class ItemHandler
 	{
+		#region Properties
+
+		protected const float InventoryFadingDuration = 1f;
+		protected float InventoryVisibilityHansel;
+		protected float InventoryVisibilityGretel;
+
+		#endregion
+
 		#region Constructor
 
 		public ItemHandler()
@@ -25,6 +36,47 @@ namespace HanselAndGretel
 		}
 
 		public void Update(SceneData pScene, Hansel pHansel, Gretel pGretel)
+		{
+			CollectItems(pScene, pHansel, pGretel);
+			ShowInventory(pHansel, pGretel);
+			UpdateInventory(pHansel, pGretel);
+			UpdateInventoryFading(pHansel, pGretel);
+		}
+
+		protected void ShowInventory(Hansel pHansel, Gretel pGretel)
+		{
+			//Hansel
+			if (pHansel.mCurrentActivity.GetType() == typeof(None) && pHansel.Input.SwitchItemJustPressed)
+			{
+				pHansel.mCurrentActivity = new SwitchItem(pHansel, pGretel);
+				pHansel.mCurrentActivity.mStateHansel = ActivityState.State.Preparing;
+			}
+			else if (pHansel.mCurrentActivity.GetType() == typeof(SwitchItem) && (pHansel.Input.SwitchItemJustPressed || pHansel.Input.BackJustPressed))
+			{
+				pHansel.mCurrentActivity = new None();
+			}
+
+			//Gretel
+			if (pGretel.mCurrentActivity.GetType() == typeof(None) && pGretel.Input.SwitchItemJustPressed)
+			{
+				pGretel.mCurrentActivity = new SwitchItem(pHansel, pGretel);
+				pGretel.mCurrentActivity.mStateGretel = ActivityState.State.Preparing;
+			}
+			else if (pGretel.mCurrentActivity.GetType() == typeof(SwitchItem) && (pGretel.Input.SwitchItemJustPressed || pGretel.Input.BackJustPressed))
+			{
+				pGretel.mCurrentActivity = new None();
+			}
+		}
+
+		protected void UpdateInventory(Hansel pHansel, Gretel pGretel)
+		{
+			if (pHansel.mCurrentActivity.GetType() == typeof(SwitchItem))
+				pHansel.mCurrentActivity.GetUpdateMethodForPlayer(pHansel)(pHansel);
+			if (pGretel.mCurrentActivity.GetType() == typeof(SwitchItem))
+				pGretel.mCurrentActivity.GetUpdateMethodForPlayer(pGretel)(pGretel);
+		}
+
+		protected void CollectItems(SceneData pScene, Hansel pHansel, Gretel pGretel)
 		{
 			foreach (Item item in pScene.Items)
 			{
@@ -45,6 +97,23 @@ namespace HanselAndGretel
 					}
 				}
 			}
+		}
+
+		protected void UpdateInventoryFading(Hansel pHansel, Gretel pGretel)
+		{
+			//Fade ButtonShowing
+			float TmpFadingDelta = InventoryFadingDuration * (float)(EngineSettings.Time.ElapsedGameTime.TotalMilliseconds / 1000d);
+			if (pHansel.mCurrentActivity.GetType() == typeof(SwitchItem)) //Fade ButtonShowingHansel in
+				InventoryVisibilityHansel += TmpFadingDelta;
+			else //Fade ButtonShowingHansel out
+				InventoryVisibilityHansel -= TmpFadingDelta;
+			if (pGretel.mCurrentActivity.GetType() == typeof(SwitchItem)) //Fade ButtonShowingGretel in
+				InventoryVisibilityGretel += TmpFadingDelta;
+			else //Fade ButtonShowingGretel out
+				InventoryVisibilityGretel -= TmpFadingDelta;
+			//Clamp ButtonShowing to 0-1
+			InventoryVisibilityHansel = MathHelper.Clamp(InventoryVisibilityHansel, 0f, 1f);
+			InventoryVisibilityGretel = MathHelper.Clamp(InventoryVisibilityGretel, 0f, 1f);
 		}
 
 		#endregion

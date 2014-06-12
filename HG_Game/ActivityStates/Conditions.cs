93930df87129a3c79f716e38b348c78936ac82d4
sensorreinigung <jens@richter-fam.de>
2014-06-12 18:05:44 +0200

    Conditions Init

---
+++ HG_Game/ActivityStates/Conditions.cs
@@ -0,0 +1,28 @@
+﻿using HanselAndGretel.Data;
+using KryptonEngine.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HG_Game
+{
+	public static class Conditions
+	{
+
+		public static bool NotHandicapped(Player pPlayer, Player pOtherPlayer, ActivityState pActivityState)
+		{
+			return true;
+		}
+
+		public static bool Intersects(Player pPlayer, Player pOtherPlayer, ActivityState pActivityState)
+		{
+			return true;
+		}
+
+		public static bool Contains(Player pPlayer, Player pOtherPlayer, ActivityState pActivityState)
+		{
+			return true;
+		}
+	}
+}

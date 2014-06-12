4be211e1b3476a43209c28800c843e19e06c7991
sensorreinigung <jens@richter-fam.de>
2014-06-12 17:43:23 +0200

    Activity added

---
+++ HG_Game/ActivityStates/BalanceOverBrokenTree.cs
@@ -0,0 +1,16 @@
+﻿using KryptonEngine.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HG_Game
+{
+	class BalanceOverBrokenTree : ActivityState
+	{
+		public BalanceOverBrokenTree(InteractiveObject pIObj)
+			: base(pIObj)
+		{
+		}
+	}
+}

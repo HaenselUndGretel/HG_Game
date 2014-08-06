14e1f7d46b9003b71c465bbf5f09f6d0d669a482
2Light <Kamui1337@googlemail.com>
2014-08-06 16:05:16 +0200

    Cutscene Intro

--- HG_Game/Scenes/SplashScene.cs
+++ HG_Game/Scenes/SplashScene.cs
@@ -9,118 +9,61 @@ using KryptonEngine.Manager;
 using Microsoft.Xna.Framework;
 using KryptonEngine.Controls;
 using Microsoft.Xna.Framework.Input;
+using KryptonEngine;
 
 namespace HG_Game
 {
-	public class SplashScene : Scene
-	{
-		#region Properties
+    public class SplashScene : Scene
+    {
+        #region Properties
 
-		protected enum LogoState
-		{
-			GA_FadeIn,
-			GA_Idle,
-			GA_FadeOut,
-			Fables_FadeIn,
-			Fables_Idle,
-			Fables_FadeOut
-		};
+        bool isBehindert = false;
 
-		protected LogoState State;
-		protected Texture2D rTextureGA;
-		protected Texture2D rTextureFables;
-		protected SteppingProgress Visibility;
-		protected SteppingProgress IdleTimer;
+        #endregion
 
-		#endregion
+        #region Constructor
 
-		#region Constructor
-
-		public SplashScene(String pSceneName)
+        public SplashScene(String pSceneName)
             : base(pSceneName)
         {
 
         }
 
-		#endregion
+        #endregion
 
-		#region Override Methods
+        #region Override Methods
 
-		public override void Initialize()
-		{
-			mCamera = new Camera();
-			State = LogoState.GA_FadeIn;
-			Visibility = new SteppingProgress(1.5f);
-			IdleTimer = new SteppingProgress(4f);
-		}
+        public override void Initialize()
+        {
+        }
 
-		public override void LoadContent()
-		{
-			rTextureGA = TextureManager.Instance.GetElementByString("logo_ga");
-			rTextureFables = TextureManager.Instance.GetElementByString("logo_fables");
-		}
+        public override void LoadContent()
+        {
+        }
 
-		public override void Update()
-		{
-			switch (State)
-			{
-				case LogoState.GA_FadeIn:
-					Visibility.StepForward();
-					if (Visibility.Complete)
-						State = LogoState.GA_Idle;
-					break;
-				case LogoState.GA_Idle:
-					IdleTimer.StepForward();
-					IdleTimer.StepForward();
-					if (IdleTimer.Complete)
-					{
-						IdleTimer.Reset();
-						State = LogoState.GA_FadeOut;
-					}
-					break;
-				case LogoState.GA_FadeOut:
-					Visibility.StepBackward();
-					if (Visibility.Progress <= 0f)
-						State = LogoState.Fables_FadeIn;
-					break;
-				case LogoState.Fables_FadeIn:
-					Visibility.StepForward();
-					if (Visibility.Complete)
-						State = LogoState.Fables_Idle;
-					break;
-				case LogoState.Fables_Idle:
-					IdleTimer.StepForward();
-					if (IdleTimer.Complete)
-					{
-						IdleTimer.Reset();
-						State = LogoState.Fables_FadeOut;
-					}
-					break;
-				case LogoState.Fables_FadeOut:
-					Visibility.StepBackward();
-					if (Visibility.Progress <= 0f)
-						SceneManager.Instance.SetCurrentSceneTo("Menu");
-					break;
-			}
-			if (InputHelper.Player1.InputJustPressed(InputHelper.mDebug))
-			{
-				Visibility.Reset();
-				IdleTimer.Reset(true);
-				SceneManager.Instance.SetCurrentSceneTo("Menu");
-			}
-		}
+        public override void Update()
+        {
+            if(!isBehindert)
+            {
+                CutScenePlayer.Play("Intro");
+                isBehindert = true;
+            }
+            if (InputHelper.Player1.InputJustPressed(InputHelper.mDebug))
+            {
+                CutScenePlayer.Stop();
+                SceneManager.Instance.SetCurrentSceneTo("Menu");
+            }
+            if (!CutScenePlayer.isPlaying()) SceneManager.Instance.SetCurrentSceneTo("Menu");
+        }
 
-		public override void Draw()
-		{
-			Texture2D Logo = rTextureGA;
-			if (State == LogoState.Fables_FadeIn || State == LogoState.Fables_Idle || State == LogoState.Fables_FadeOut)
-				Logo = rTextureFables;
-			mSpriteBatch.Begin();
-			mSpriteBatch.Draw(Logo, Vector2.Zero, Color.White);
-			mSpriteBatch.Draw(TextureManager.Instance.GetElementByString("pixel"), new Rectangle(0, 0, 1280, 720), Color.Black * Visibility.ProgressInverse);
-			mSpriteBatch.End();
-		}
+        public override void Draw()
+        {
+            
+            mSpriteBatch.Begin();
+            CutScenePlayer.Draw(mSpriteBatch);
+            mSpriteBatch.End();
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
